3c9834872e5e34f8700fbd911a068b08f23c80ad
marciodscampos <marciodscampos@gmail.com>
2015-09-29 18:37:05 -0300

    3 heurística terminada

--- src/Graph.java
+++ src/Graph.java
@@ -40,10 +40,12 @@ public class Graph {
                 simpleRecursiveBacktracking(vertexes);
                 break;
             case "b":
-                System.out.println("Backtracking com verificação adiante");
+                // Backtracking com verificação adiante
+                RecursiveBacktrackingWithForwardChecking(vertexes);
                 break;
             case "c":
-                System.out.println("Backtracking com verificação adiante e MVR");
+                //Backtracking com verificação adiante e MVR
+                RecursiveBacktrackingWithForwardCheckingMVR(vertexes);
                 break;
             case "d":
                 System.out.println("Backtracking com verificação adiante, MVR e grau");
@@ -52,7 +54,7 @@ public class Graph {
         return true;
     }
 
-    public boolean simpleRecursiveBacktracking(ArrayList< Vertex> assignments) {
+    public boolean simpleRecursiveBacktracking(ArrayList<Vertex> assignments) {
         if (assignments.stream().filter(s -> s.getColor() == 0).count() == 0) // Se a solução foi encontrada
             return true;
 
@@ -76,6 +78,70 @@ public class Graph {
                 return false;               // Se existe um adjacente com a mesma atribuição, retorna false
         }
         return true;    // Caso contrário, retorna true
+    }
+
+    public boolean RecursiveBacktrackingWithForwardChecking(ArrayList<Vertex> assignments){
+        if (assignments.stream().filter(s -> s.getColor() == 0).count() == 0) // Se a solução foi encontrada
+            return true;
+
+        Vertex var = assignments.stream().filter(s -> s.getColor() == 0).findFirst().get(); // Seleciona uma variável sem atribuição
+        for (int color = 1; color < 5; color++) {         // Para cada elemento do domínio
+            if (var.isValidColor(color) && checkRemainingValues(var, color)){// Verfica se alguma variável ficará sem valores legais
+                var.setColor(color);                // Atribui o valor à variável
+                updateRemainingColor(var, color, false);   // Atualiza as cores válidas remanescentes
+                Boolean success = RecursiveBacktrackingWithForwardChecking(assignments); // Chama o método recursivamente
+                if (success) return true;           // Se houve exito retorna true
+                var.setColor(0);                    // Caso contrário, remove a atribuição
+                updateRemainingColor(var, color, true);   // Atualiza as cores válidas remanescentes
+            }
+        }
+        return false; // Retorna false, pois n encontrou uma atribuição válida
+    }
+
+    // verifica se existem valores remanescentes válidos para todas as variáveis
+    public boolean checkRemainingValues(Vertex vertex, int color) {
+        for (int i = 0; i < vertex.getE(); i++) {                   // Para cada vertice adjacente
+            String edgeName = vertex.getEdges().get(i).getName();   // Copia o nome do vértice destino
+            if (vertexes.stream().filter(s->s.getName().compareTo(edgeName) == 0).findFirst().get().isOnlyRemainingColor(color))
+                return false;               // verifica se a cor atribuida para var é a unica possível para algum adjacente
+        }
+        return true;    // Caso contrário, retorna true
+    }
+
+    // Atualiza as cores remanescentes válidas
+    public void updateRemainingColor(Vertex vertex, int color, boolean addFlag){
+        for (int i = 0; i < vertex.getE(); i++) {                   // Para cada vertice adjacente
+            String edgeName = vertex.getEdges().get(i).getName();   // Copia o nome do vértice destino
+            if(addFlag)
+                vertexes.stream().filter(s ->s.getName().compareTo(edgeName) == 0).findFirst().get().setColorValid(color);
+                // Coloca como válida a cor 'color' em todos os vértices adjacentes de vertex
+            else
+                vertexes.stream().filter(s ->s.getName().compareTo(edgeName) == 0).findFirst().get().setColorInvalid(color);
+                // Coloca como inválida a cor 'color' em todos os vértices adjacentes de vetex
+        }
+    }
+
+    public boolean RecursiveBacktrackingWithForwardCheckingMVR(ArrayList<Vertex> assignments){
+        if (assignments.stream().filter(s -> s.getColor() == 0).count() == 0) // Se a solução foi encontrada
+            return true;
+
+        // Seleciona uma variável sem atribuição
+        Vertex var = assignments.stream().filter(s -> s.getColor() == 0).sorted((s1, s2)-> {
+            return s1.getNumberOfRemainingColors() - s2.getNumberOfRemainingColors();
+        }).findFirst().get();
+        // Nesse caso escolhemos a primeira com o menor número de valores remanescente ()
+
+        for (int color = 1; color < 5; color++) {         // Para cada elemento do domínio
+            if (var.isValidColor(color) && checkRemainingValues(var, color)){// Verfica se alguma variável ficará sem valores legais
+                var.setColor(color);                // Atribui o valor à variável
+                updateRemainingColor(var, color, false);   // Atualiza as cores válidas remanescentes
+                Boolean success = RecursiveBacktrackingWithForwardChecking(assignments); // Chama o método recursivamente
+                if (success) return true;           // Se houve exito retorna true
+                var.setColor(0);                    // Caso contrário, remove a atribuição
+                updateRemainingColor(var, color, true);   // Atualiza as cores válidas remanescentes
+            }
+        }
+        return false; // Retorna false, pois n encontrou uma atribuição válida
     }
 
     public void printGraph(){
--- src/Vertex.java
+++ src/Vertex.java
@@ -9,11 +9,15 @@ public class Vertex {
     int index;
     ArrayList<Edge> edges;
     int E;
+    int remainingColors[];
 
     Vertex(String name, int index) {
         this.name = name;
         color = 0;
         this.index = index;
+        remainingColors = new int[5];
+        for (int i = 0; i < 5; i++)
+            remainingColors[i] = 1;
     }
 
     public void addEdges(int E, String[] names){
@@ -48,5 +52,36 @@ public class Vertex {
 
     public ArrayList<Edge> getEdges() {
         return edges;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public boolean isValidColor(int color) {
+        return remainingColors[color] == 1;
+    }
+
+    public void setColorValid(int color){
+        remainingColors[color] = 1;
+    }
+
+    public  void setColorInvalid(int color){
+        remainingColors[color] = 0;
+    }
+
+    public boolean isOnlyRemainingColor(int color){
+        for (int i = 1; i < 5; i++){
+            if ((i != color && isValidColor(i)) || (i == color && !isValidColor(color)))
+                return false;
+        }
+        return true;
+    }
+
+    public int getNumberOfRemainingColors() {
+        int sum = 0;
+        for (int i = 1; i < 5; i++)
+            sum = sum + i;
+        return sum;
     }
 }
