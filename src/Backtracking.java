2800bfbabcce24c19102ed8c2855193d6075eace
marciodscampos <marciodscampos@gmail.com>
2015-09-30 17:04:52 -0300

    Leitura por arquivo

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,27 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
  * Created by Marcio on 29/09/2015.
  */
-public class main {
+public class Backtracking {
     public static void main(String args[]){
-
+        //Le o nome de um arquivo de entrada
         Scanner input = new Scanner(System.in);
+        String fileName = input.nextLine();
+
+        // Abre um arquivo pra leitura
+        File file = new File(fileName);
+        Scanner fileReader;
+        try {
+            fileReader = new Scanner(file, "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao abrir arquivo!");
+            return;
+        }
 
         // Leitura do numero de regiões do mapa e da heurística a ser utilizada
-        int size = input.nextInt();
-        String heuristic = input.next();
+        int size = fileReader.nextInt();
+        String heuristic = fileReader.next();
 
         String[] vertexes = new String[size];
         String[][] edges = new String[size][];
 
         // Leitura das entradas
         for (int i = 0; i < size; i++) {
-            input.useDelimiter(":");                                // Define ':' como delimitador
-            vertexes[i] = input.next();                             // Lê o nome da região do mapa
+            fileReader.useDelimiter(":");                                // Define ':' como delimitador
+            vertexes[i] = fileReader.next();                             // Lê o nome da região do mapa
             if (i == 0 && vertexes[i].charAt(0) == '\n')
                 vertexes[i] = vertexes[i].substring(1);             // Trata o caso do '\n' inicial
-            edges[i] = input.nextLine().substring(1).split(",");    // Lê o restante da linha e separa em strings
+            edges[i] = fileReader.nextLine().substring(1).split(",");    // Lê o restante da linha e separa em strings
 
             // Formata os nomes
             for (int j = 0; j < edges[i].length; j++) {
